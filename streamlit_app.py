01a512ce617bf4fcc5c583a9b9b72b5d4c0112b3
Mayank Harlalka <35811255+yourstrulyMayank@users.noreply.github.com>
2023-04-24 11:36:57 +0530

    Update streamlit_app.py
    Trying to find if secrets file works

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit
 import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
+CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 # import pandas
 # streamlit.title('Zena\'s Amazing Athleisure Catalog')
 # # connect to snowflake
